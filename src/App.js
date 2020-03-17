77f751f0f07573b8d1e2bd40636655326e0685e7
nafis195 <nafisbuet1@gmail.com>
2020-03-16 19:11:54 -0700

    firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+
+firebase.initializeApp(firebaseConfig)
+
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+      .then(res => {
+        const { displayName, photoURL, email } = res.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(signedInUser);
+        console.log(displayName, email, photoURL);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+      })
+  }
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+    .then(res => {
+      const signedOutUser = {
+        isSignedIn: false,
+        name: '',
+        photo: '',
+        email: '',
+        password: ''
+      }
+      setUser(signedOutUser);
+      console.log(res);
+    })
+    .catch(err => {
+
+    })
+  }
+
+  const handleChange = e => {
+    const newUserInfo = {
+      ...user
+    }
+    newUserInfo[e.target.name] = e.target.value;
+    setUser(newUserInfo);
+  }
+
+  const createAccount = () => {
+    console.log(user.email, user.password);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.isSignedIn ? <button onClick={handleSignOut}>Sign Out</button> : <button onClick={handleSignIn}>Sign in</button>
+        
+      }
+      {
+        user.isSignedIn && <div>
+          <p>Welcome, {user.name}</p>
+          <p>Your email: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
+      <h1>Out Own Auth</h1>
+      <input type="text" onBlur={handleChange} name="email" placeholder = "Your email"/>
+      <br/>
+      <input type="password" onBlur={handleChange} name="password" placeholder="password"/>
+      <br/>
+      <button onClick={createAccount}>Create Account</button>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDSP4KkjQnrprrobWQZMQnw7vJJy4IWlKE",
+    authDomain: "ema-john-simple-nafis195.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-nafis195.firebaseio.com",
+    projectId: "ema-john-simple-nafis195",
+    storageBucket: "ema-john-simple-nafis195.appspot.com",
+    messagingSenderId: "1043763794328",
+    appId: "1:1043763794328:web:29e1ed1604f08481d5dbed"
+  };
+
+  export default firebaseConfig;
